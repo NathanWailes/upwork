63ad21589fae677795ae207badbc0f5a7b776ca4
LAPTOP-L8343ATF\Nathan Wailes <nathan.wailes@gmail.com>
2018-01-11 20:18:40 -0800

    Help someone fix an OOM error when downloading data from Mixpanel. They just needed to save the data as it was coming in rather than trying to first hold it all in memory.

---
+++ 2018/01/oom_error/README.md
@@ -0,0 +1,7 @@
+#### Notes from Nathan:
+
+- 99% of the code here isn't mine. I just changed a few lines.
+- The "people" program was working fine. The "events" program was the one crashing. I fixed
+it by switching it to use urllib.urlretrieve(), which I learned about by just doing a
+Google search and seeing an answer on Stack Overflow.
+- You can see what I did by comparing the "raw_events...py" and "raw_events...fixed.py" files.
---
+++ 2018/01/oom_error/export_raw_events_hiring-DEV_fixed.py
@@ -0,0 +1,122 @@
+#! /usr/bin/env python
+#
+# Mixpanel, Inc. -- http://mixpanel.com/
+#
+# Python API client library to consume mixpanel.com analytics data.
+
+import hashlib
+import urllib
+import pickle
+import time
+
+from datetime import date, timedelta
+
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+
+class Mixpanel(object):
+
+	ENDPOINT = 'http://data.mixpanel.com/api'
+	VERSION = '2.0'
+
+	def __init__(self, api_key, api_secret):
+		self.api_key = api_key
+		self.api_secret = api_secret
+		
+	def request(self, methods, params, output_filepath):
+		"""
+			methods - List of methods to be joined, e.g. ['events', 'properties', 'values']
+					  will give us http://mixpanel.com/api/2.0/events/properties/values/
+			params - Extra parameters associated with method
+		"""
+		params['api_key'] = self.api_key
+		params['expire'] = int(time.time()) + 600   # Grant this request 10 minutes.
+
+		if 'sig' in params: del params['sig']
+		params['sig'] = self.hash_args(params)
+
+		request_url = '/'.join([self.ENDPOINT, str(self.VERSION)] + methods) + '/?' + self.unicode_urlencode(params)
+	
+		print request_url
+		
+		request = urllib.urlretrieve(request_url, output_filepath)
+
+
+	def unicode_urlencode(self, params):
+		"""
+			Convert lists to JSON encoded strings, and correctly handle any 
+			unicode URL parameters.
+		"""
+		if isinstance(params, dict):
+			params = params.items()
+		for i, param in enumerate(params):
+			if isinstance(param[1], list): 
+				params[i] = (param[0], json.dumps(param[1]),)
+
+		return urllib.urlencode(
+			[(k, isinstance(v, unicode) and v.encode('utf-8') or v) for k, v in params]
+		)
+
+	def hash_args(self, args, secret=None):
+		"""
+			Hashes arguments by joining key=value pairs, appending a secret, and 
+			then taking the MD5 hex digest.
+		"""
+		for a in args:
+			if isinstance(args[a], list): args[a] = json.dumps(args[a])
+
+		args_joined = ''
+		for a in sorted(args.keys()):
+			if isinstance(a, unicode):
+				args_joined += a.encode('utf-8')
+			else:
+				args_joined += str(a)
+
+			args_joined += '='
+
+			if isinstance(args[a], unicode):
+				args_joined += args[a].encode('utf-8')
+			else:
+				args_joined += str(args[a])
+
+		hash = hashlib.md5(args_joined)
+		print args_joined
+
+		if secret:
+			hash.update(secret)
+		elif self.api_secret:
+			hash.update(self.api_secret)
+		return hash.hexdigest() 
+
+api_key = '<<string>>'
+api_secret = '<<string>>'
+#event_array = raw_input("Events (array): ")
+#from_date = raw_input("From Date: ")
+#to_date = raw_input("To Date: ")
+#where = raw_input("where (optional): ")
+
+lastweek=date.today()-timedelta(days=2)
+fromdate=str(lastweek)
+thisday=date.today()
+todate=str(thisday)
+
+api = Mixpanel(
+	api_key = api_key, 
+	api_secret = api_secret
+)
+
+api.request(['export'], {
+	# 'event': [''],
+	# 'format': 'csv',
+	#'interval':1,
+	#'type': 'unique',
+	#'unit': 'day'
+	'from_date': fromdate,
+	'to_date': todate
+	#'where': ''
+	}, output_filepath="hiring.out")
+	
+print data
